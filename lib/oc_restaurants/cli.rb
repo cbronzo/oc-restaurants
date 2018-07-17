4b318bc9169a603c73eece59e18e4f97e686c226
cbronzo <cbronzo@mac.com>
2018-07-16 21:05:28 -0700

    Building out full-functioning CLI

--- lib/oc_restaurants/cli.rb
+++ lib/oc_restaurants/cli.rb
@@ -8,19 +8,73 @@ class CLI
   end
 
   def start
-    puts "Welcome to Orange County's Best Restaurant
-   Lineup!"
-   # main_menu
-   Scraper.new.scrape_restaurants
-   list_restaurants
+    puts "Welcome to 'Orange County's 21 Hottest Restaurants' Guide!"
+    Scraper.new.scrape_restaurants
+    menu
   end
 
-  def list_restaurants
-    puts "Please select a restaurant to learn more!"
-    Best_Restaurant.all.each.with_index(1) do |restaurant, index|
+  def menu
+    puts "Type 'guide' to see the list of OC Restaurants."
+    puts "Otherwise, type 'exit'."
+    input = gets.strip
+      if input == "guide"
+        list_restaurant_name
+      elsif input == "exit"
+        puts "You are now exiting the program. Thank you, goodbye!"
+        exit
+      else
+        error
+        menu
+      end
+  end
 
-      puts "#{index}. #{restaurant.website}"
+
+  def list_restaurant_name
+    puts "Here are 21 of the OC's best restaurants!"
+    puts "Please select a restaurant by entering its number (1-21) to learn more!"
+    Best_Restaurant.all.each.with_index(1) do |restaurant, index|
+      puts "#{index}. #{restaurant.name}"
     end
+    input = gets.strip
+    index = input.to_i
+      if input == 1..21
+        restaurant_attributes
+      else error
+        list_restaurant_name
+  end
+
+  def input
+    input == gets.strip
+
+  end
+
+  def restaurant_attributes(input)
+    input ==
+
+    Best_Restaurant.all.each do |restaurant|
+      puts "#{restaurant.name}"
+      puts "#{restaurant.description}"
+      puts "#{restaurant.address}"
+      puts "#{restaurant.phone}"
+      puts "#{restaurant.website}"
+    end
+  end
+
+
+  def list_restaurant_description
+  end
+
+  def list_restaurant_phone
+  end
+
+  def list_restaurant_address
+  end
+
+  def list_restaurant_website
+  end
+
+  def error
+    puts "~Error, please try again~"
   end
 
 end
