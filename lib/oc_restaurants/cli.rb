59f9b753e46a4583484404919ca3ca54f0af7312
cbronzo <cbronzo@mac.com>
2018-07-16 22:10:31 -0700

    Adding final methods to CLI

--- lib/oc_restaurants/cli.rb
+++ lib/oc_restaurants/cli.rb
@@ -13,6 +13,7 @@ class CLI
     menu
   end
 
+
   def menu
     puts "Type 'guide' to see the list of OC Restaurants."
     puts "Otherwise, type 'exit'."
@@ -30,51 +31,67 @@ class CLI
 
 
   def list_restaurant_name
-    puts "Here are 21 of the OC's best restaurants!"
-    puts "Please select a restaurant by entering its number (1-21) to learn more!"
+    puts "YUM! Here are 21 of the OC's best & hottest restaurants!"
     Best_Restaurant.all.each.with_index(1) do |restaurant, index|
       puts "#{index}. #{restaurant.name}"
     end
+    puts "Please select a restaurant by entering its number (1-21) to learn more!"
+    puts "To exit, type 'exit'."
     input = gets.strip
-    index = input.to_i
-      if input == 1..21
+      if input.to_i.between?(1,21)
+        index = input.to_i - 1
         restaurant_attributes
-      else error
+      elsif input == "exit"
+        puts "You are now exiting the program. Thank you, goodbye!"
+        exit
+      else
+        error
         list_restaurant_name
-  end
-
-  def input
-    input == gets.strip
+      end
 
   end
 
-  def restaurant_attributes(input)
-    input ==
 
-    Best_Restaurant.all.each do |restaurant|
-      puts "#{restaurant.name}"
-      puts "#{restaurant.description}"
-      puts "#{restaurant.address}"
-      puts "#{restaurant.phone}"
-      puts "#{restaurant.website}"
-    end
+  def restaurant_attributes
+    puts "This is working."
+    # Best_Restaurant.all
+    # puts "Below, you will find the details for #{restaurant.name}."
+    #
+    #
+    #   puts "Description: #{restaurant.description}"
+    #     if restaurant.address == ""
+    #       puts "Sorry, no address information available."
+    #     else
+    #       puts "Address: #{restaurant.address}"
+    #     end
+    #
+    #     if restaurant.phone == ""
+    #       puts "Sorry, no phone number available."
+    #     else
+    #       puts "Phone Number: #{restaurant.phone}"
+    #     end
+    #
+    #     if restaurant.website == ""
+    #       puts "Sorry, no website available."
+    #     else
+    #       puts "Website: #{restaurant.website}"
+    #     end
+    #
+    #   puts "To go back to the guide, type 'back'."
+    #   puts "To exit, type 'exit'."
+    #   input = gets.strip
+    #     if input == "back"
+    #       list_restaurant_name
+    #     elsif input == "exit"
+    #       exit
+    #     else
+    #       error
+    #     end
   end
 
-
-  def list_restaurant_description
-  end
-
-  def list_restaurant_phone
-  end
-
-  def list_restaurant_address
-  end
-
-  def list_restaurant_website
-  end
 
   def error
-    puts "~Error, please try again~"
+    puts "~ERROR, PLEASE TRY AGAIN~"
   end
 
 end
